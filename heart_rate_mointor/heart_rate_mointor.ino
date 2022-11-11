53db1212555d3e9613388c30416b70fc5a211c71
joeadham <yousefdhm@gmail.com>
2022-11-11 18:05:37 +0200

    Displayed the signal on webpage

--- heart_rate_mointor/heart_rate_mointor.ino
+++ heart_rate_mointor/heart_rate_mointor.ino
@@ -1,54 +1,24 @@
-#include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
+int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
+int fsrReading;     // the analog reading from the FSR resistor divider
  
-#define REPORTING_PERIOD_MS     1000
- 
-PulseOximeter pox;
-uint32_t tsLastReport = 0;
- 
-void onBeatDetected()
-{
-    Serial.println("Beat!");
+void setup(void) {
+  Serial.begin(9600);   
 }
  
-void setup()
-{
-    Serial.begin(115200);
-    Serial.print("Initializing pulse oximeter..");
- 
-    // Initialize the PulseOximeter instance
-    // Failures are generally due to an improper I2C wiring, missing power supply
-    // or wrong target chip
-    if (!pox.begin()) {
-        Serial.println("FAILED");
-        for(;;);
-    } else {
-        Serial.println("SUCCESS");
-    }
-     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
- 
-    // Register a callback for the beat detection
-    pox.setOnBeatDetectedCallback(onBeatDetected);
-}
- 
-void loop()
-{
-    // Sending data
-    float dataRead = analogRead(A0);
+void loop(void) {
+  float dataRead = analogRead(fsrPin);
+//  fsrReading = analogRead(fsrPin);  
+// 
+//  Serial.println("Analog reading = ");
+//  Serial.println(fsrReading);     // print the raw analog reading
+
+   
     dataRead = (dataRead/2024.0) * 0.5;
-//    String dataToSend = String(dataRead);
     Serial.println(dataRead);
     delay(300);
-    
-    // Make sure to call update as fast as possible
-    pox.update();
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-        Serial.print("Heart rate:");
-        Serial.print(pox.getHeartRate());
-        Serial.print("bpm / SpO2:");
-        Serial.print(pox.getSpO2());
-        Serial.println("%");
  
-        tsLastReport = millis();
-    }
+  
+   
+
+  delay(1000);
 }
--- heart_rate_mointor/main2.py
+++ heart_rate_mointor/main2.py
@@ -1,25 +1,86 @@
+import streamlit as st
 import serial
 import matplotlib.pyplot as plt
-import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-plt.ion()
-fig = plt.figure()
+ 
 
-i = 0
-x = list()
-y = list()
-ser = serial.Serial('com3' , 9600)
-ser.close()
-ser.open()
+with st.sidebar:
+    radio1  = st.radio(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+   
 
-while True:
-    data = ser.readline()
-    print(data.decode())
-    x.append(i)
-    y.append(data.decode())
+
+
+
+
+
+if radio1 == "Comedy":
+    # The variable that plots the graph
+    graph_placeholder = st.empty()
+
+
+
+    def plotting(x,y,placeholder):
+        """
+        Draws/updates the graph
+
+        Arguments:
+            x: x axis value
+            y: y axis value
+        """
+        fig = go.Figure()
     
+        fig.add_trace(go.Scatter(x=x,y=y))
 
-    plt.scatter(i , float(data.decode()))
-    i += 1
-    plt.show()
-    plt.pause(0.0001)
+        placeholder.write(fig)
+    
+    ## Initializing the variables
+    counter = 0
+    x_axis = list()
+    y_axis = list()
+
+    try:
+        serial_object = serial.Serial('com3' , 9600)
+        serial_object.close()
+        serial_object.open()
+    except:
+        pass    
+    flag = True
+    while radio1 == "Comedy":
+        
+
+            try:
+                serial_object.flushInput()
+                serial_object.flushOutput()
+                serial_object.flush()
+                data = serial_object.readline()
+                print(data.decode())
+                ## concatenating the value of x and y to the list
+                x_axis.append(counter)
+                y_axis.append(data.decode())
+
+                counter += 1
+                plotting(x_axis,y_axis,graph_placeholder)
+                # if counter == 5:
+                #     graph_placeholder.empty()
+                #     break
+                checbox = st.sidebar.checkbox("test")
+                if checbox:
+                    radio1 = "Drama"
+                st.write(radio1)
+                st.write(counter)
+                
+            except:
+                pass
+
+            
+            plt.pause(0.0001)
+        
+
+
+elif radio1 == "Drama":
+    st.write("HELOOOOOOOO")
+
