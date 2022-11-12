8a8e555bf2e09cd6b638495087317946cbb376ef
joeadham <yousefdhm@gmail.com>
2022-11-13 00:08:32 +0200

    testing breaking out of the infinite loop

--- heart_rate_mointor/main2.py
+++ heart_rate_mointor/main2.py
@@ -3,8 +3,72 @@ import serial
 import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import threading
 
- 
+graph_placeholder = st.empty()
+class setInterval():
+    def _init_(self, func, sec):
+        def func_wrapper():
+            self.t = threading.Timer(sec, func_wrapper)
+            self.t.start()
+            func()
+        self.t = threading.Timer(sec, func_wrapper)
+        self.t.start()
+
+    def cancel(self):
+        self.t.cancel()
+   
+def test ():
+        def plotting(x,y,placeholder):
+       
+            fig = go.Figure()
+        
+            fig.add_trace(go.Scatter(x=x,y=y))
+
+            placeholder.write(fig)
+    
+    ## Initializing the variables
+        counter = 0
+        x_axis = list()
+        y_axis = list()
+
+        try:
+            serial_object = serial.Serial('com3' , 9600)
+            serial_object.close()
+            serial_object.open()
+        except:
+            pass    
+        flag = True
+        
+            
+
+        try:
+                    serial_object.flushInput()
+                    serial_object.flushOutput()
+                    serial_object.flush()
+                    data = serial_object.readline()
+                    print(data.decode())
+                    ## concatenating the value of x and y to the list
+                    x_axis.append(counter)
+                    y_axis.append(data.decode())
+
+                    counter += 1
+                    plotting(x_axis,y_axis,graph_placeholder)
+                    # if counter == 5:
+                    #     graph_placeholder.empty()
+                    #     break
+                    checbox = st.sidebar.checkbox("test")
+                    if checbox:
+                        radio1 = "Drama"
+                    st.write(radio1)
+                    st.write(counter)
+                    
+        except:
+                    pass
+
+                
+        plt.pause(0.0001)
+
 
 with st.sidebar:
     radio1  = st.radio(
@@ -12,75 +76,23 @@ with st.sidebar:
     ('Comedy', 'Drama', 'Documentary'))
    
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 
 
 
 
-if radio1 == "Comedy":
-    # The variable that plots the graph
-    graph_placeholder = st.empty()
 
+with tab1:
 
-
-    def plotting(x,y,placeholder):
-        """
-        Draws/updates the graph
-
-        Arguments:
-            x: x axis value
-            y: y axis value
-        """
-        fig = go.Figure()
-    
-        fig.add_trace(go.Scatter(x=x,y=y))
-
-        placeholder.write(fig)
-    
-    ## Initializing the variables
-    counter = 0
-    x_axis = list()
-    y_axis = list()
-
-    try:
-        serial_object = serial.Serial('com3' , 9600)
-        serial_object.close()
-        serial_object.open()
-    except:
-        pass    
-    flag = True
-    while radio1 == "Comedy":
-        
-
-            try:
-                serial_object.flushInput()
-                serial_object.flushOutput()
-                serial_object.flush()
-                data = serial_object.readline()
-                print(data.decode())
-                ## concatenating the value of x and y to the list
-                x_axis.append(counter)
-                y_axis.append(data.decode())
-
-                counter += 1
-                plotting(x_axis,y_axis,graph_placeholder)
-                # if counter == 5:
-                #     graph_placeholder.empty()
-                #     break
-                checbox = st.sidebar.checkbox("test")
-                if checbox:
-                    radio1 = "Drama"
-                st.write(radio1)
-                st.write(counter)
+            okokok = threading.Timer(0.5, test())
+            okokok.start()
                 
-            except:
-                pass
-
-            
-            plt.pause(0.0001)
-        
 
 
-elif radio1 == "Drama":
-    st.write("HELOOOOOOOO")
+
+
+with tab2:
+    okokok.cancel()
+    st.write("Helloooooooo")
 
