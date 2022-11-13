ac968b9b75969510336533d65fe62cd58a2a9c42
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-11-13 23:41:59 +0200

    dynamic graph is done

--- heart_rate_mointor/pages/EMG.py
+++ heart_rate_mointor/pages/EMG.py
@@ -7,61 +7,41 @@ class variables:
     start      = 0
     graph_size = 10
 
-start_btn  = st.sidebar.button(label='Start' )
-pause_btn  = st.sidebar.button(label='Pause' )
-resume_btn = st.sidebar.button(label='resume')
+start_btn  = st.sidebar.button(label='Show Graph Dynamically')
 
-df = pd.read_csv('../files/EMG_Dataset.csv')
+uploaded_file = st.sidebar.file_uploader(label='')
 
-signal_x_axis = (df.iloc[:,0]).to_numpy() # dataframe x axis
-signal_y_axis = (df.iloc[:,1]).to_numpy() # dataframe y axis
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-#-------------------------------------- Plot Dynamic ----------------------------------------------------
-def plot_animation(df):
-    brush  = alt.selection_interval ()
-    chart1 = alt.Chart(df).mark_line().encode(x=alt.X('time', axis=alt.Axis(title='Time')),).properties(width=414, height=250).add_selection(brush).interactive()
-    figure = chart1.encode(y=alt.Y('amplitude',axis=alt.Axis(title='Amplitude')))
-    return figure
+    signal_x_axis = (df.iloc[:,0]).to_numpy() # dataframe x axis
+    signal_y_axis = (df.iloc[:,1]).to_numpy() # dataframe y axis
 
-def Dynamic_graph(signal_x_axis, signal_y_axis,start_btn,pause_btn,resume_btn):
-    df = pd.DataFrame({'time': signal_x_axis[::200], 'amplitude': signal_y_axis[:: 200]}, columns=['time', 'amplitude'])
+    # -------------------------------------- Plot Dynamic ----------------------------------------------------
+    def plot_animation(df):
+        brush  = alt.selection_interval ()
+        chart1 = alt.Chart(df).mark_line().encode(x=alt.X('time', axis=alt.Axis(title='Time')),).properties(width=800, height=500).add_selection(brush).interactive()
+        figure = chart1.encode(y=alt.Y('amplitude',axis=alt.Axis(title='Amplitude')))
+        return figure
 
-    lines     = plot_animation    (df)
-    line_plot = st.altair_chart(lines)
+    def Dynamic_graph(signal_x_axis, signal_y_axis, start_btn):
 
-    N     = df.shape[0]  # number of elements in the dataframe
-    burst = 10           # number of elements  to add to the plot
-    size  = burst        # size of the current dataset
+        df = pd.DataFrame({'time': signal_x_axis[::15], 'amplitude': signal_y_axis[:: 15]}, columns=['time', 'amplitude'])
 
-    if start_btn:
-        for i in range(1, N):
-            variables.start      = i
-            step_df              = df.iloc[0:size]
-            lines                = plot_animation      (step_df)
-            line_plot            = line_plot.altair_chart(lines)
-            variables.graph_size = size
-            size                 = i * burst 
+        lines     = plot_animation    (df)
+        line_plot = st.altair_chart(lines)
 
-    if resume_btn: 
-        for i in range( variables.start,N):
-            variables.start      = i
-            step_df              = df.iloc[0:size]
-            lines                = plot_animation      (step_df)
-            line_plot            = line_plot.altair_chart(lines)
-            variables.graph_size = size
-            size                 = i * burst
+        N     = df.shape[0]  # number of elements in the dataframe
+        burst = 10           # number of elements  to add to the plot
+        size  = burst        # size of the current dataset
 
-    if pause_btn:
-        step_df   = df.iloc[0:variables.graph_size]
-        lines     = plot_animation      (step_df)
-        line_plot = line_plot.altair_chart(lines)
+        if start_btn:
+            for i in range(1, N):
+                variables.start      = i
+                step_df              = df.iloc[0:size]
+                lines                = plot_animation      (step_df)
+                line_plot            = line_plot.altair_chart(lines)
+                variables.graph_size = size
+                size                 = i * burst 
 
-def plot(x, y):
-        fig, axs = plt.subplots()
-        fig.set_size_inches(6,3)
-        axs.plot(x,y)
-        st.pyplot(fig)
-
-plot(signal_x_axis,signal_y_axis)
-
-Dynamic_graph(signal_x_axis, signal_y_axis, start_btn, pause_btn, resume_btn)
+    Dynamic_graph(signal_x_axis, signal_y_axis, start_btn)
