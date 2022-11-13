628c709aa6bb0390279122f34521e656fa7f47cf
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-11-13 12:21:47 +0200

    .

--- heart_rate_mointor/main2.py
+++ heart_rate_mointor/main2.py
@@ -4,33 +4,18 @@ import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
- 
 
 with st.sidebar:
     radio1  = st.radio(
     "What's your favorite movie genre",
     ('Comedy', 'Drama', 'Documentary'))
-   
-
-
-
-
-
 
 if radio1 == "Comedy":
     # The variable that plots the graph
     graph_placeholder = st.empty()
 
-
-
     def plotting(x,y,placeholder):
-        """
-        Draws/updates the graph
 
-        Arguments:
-            x: x axis value
-            y: y axis value
-        """
         fig = go.Figure()
     
         fig.add_trace(go.Scatter(x=x,y=y))
@@ -50,37 +35,34 @@ if radio1 == "Comedy":
         pass    
     flag = True
     while radio1 == "Comedy":
-        
+    
+        try:
+            serial_object.flushInput()
+            serial_object.flushOutput()
+            serial_object.flush()
+            data = serial_object.readline()
+            print(data.decode())
+            ## concatenating the value of x and y to the list
+            x_axis.append(counter)
+            y_axis.append(data.decode())
 
-            try:
-                serial_object.flushInput()
-                serial_object.flushOutput()
-                serial_object.flush()
-                data = serial_object.readline()
-                print(data.decode())
-                ## concatenating the value of x and y to the list
-                x_axis.append(counter)
-                y_axis.append(data.decode())
-
-                counter += 1
-                plotting(x_axis,y_axis,graph_placeholder)
-                # if counter == 5:
-                #     graph_placeholder.empty()
-                #     break
-                checbox = st.sidebar.checkbox("test")
-                if checbox:
-                    radio1 = "Drama"
-                st.write(radio1)
-                st.write(counter)
-                
-            except:
-                pass
+            counter += 1
+            plotting(x_axis,y_axis,graph_placeholder)
+            # if counter == 5:
+            #     graph_placeholder.empty()
+            #     break
+            checbox = st.sidebar.checkbox("test")
+            if checbox:
+                radio1 = "Drama"
+            st.write(radio1)
+            st.write(counter)
+            
+        except:
+            pass
 
             
             plt.pause(0.0001)
         
-
-
 elif radio1 == "Drama":
     st.write("HELOOOOOOOO")
 
